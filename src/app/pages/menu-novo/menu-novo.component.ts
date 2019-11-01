65138f1dca108c473bd3c960f1f07d16bc057fc6
shalom fernando <shalonando@gmail.com>
2019-11-01 20:51:18 -0300

    Deploy

---
+++ src/app/pages/menu-novo/menu-novo.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-novo',
+  templateUrl: './menu-novo.component.html',
+  styleUrls: ['./menu-novo.component.scss']
+})
+export class MenuNovoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // $('#sidebarCollapse').on('click', function () {
+  //   $('#sidebar').toggleClass('active');
+  // });
+
+  toggle(){
+
+  }
+}
