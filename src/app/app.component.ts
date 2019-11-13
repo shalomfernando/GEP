dc3f72071e592b2813e6af7ac0bc3fdc728b7c20
Daniel Oliveira <odsdaniel99@gmail.com>
2019-11-12 21:38:07 -0300

    Tradução grid.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,49 @@ import {Router} from '@angular/router';
 import {AuthenticationService} from './_services';
 import {User} from './_models';
 
+import {L10n, setCulture} from '@syncfusion/ej2-base';
+
+setCulture('pt-br');
+
+L10n.load({
+  'pt-br': {
+    'grid': {
+      'EmptyRecord': 'Nenhum registro para exibir',
+      'EmptyDataSourceError': 'A fonte de dados deve ficar em branco no carregamento inicial, pois as colunas da fonte de dados são criadas na coluna Gerada automaticamente da grade',
+      'GroupDropArea': 'Arraste um cabeçalho de coluna aqui para a coluna do grupo',
+      'UnGroup': 'Clique aqui para desagrupar',
+      'Item': 'Artigo',
+      'Items': 'Artigo',
+      'StartsWith':'Começa com',
+      'EndsWith':'Termina com',
+      'Contains':'Contém',
+      'Equal':'Igual',
+      'NotEqual':'Não igual',
+      'LessThan':'Menor que',
+      'LessThanOrEqual':'Menor ou igual',
+      'GreaterThan':'Maior que',
+      'GreaterThanOrEqual':'Maior que ou igual',
+      'ChooseDate':'Escolha a data',
+      'EnterValue':'Digite o valor',
+      'Copy':'Cópia de',
+      'FilterMenu':'Menu Filtro',
+      'ClearFilter':'Filtro limpo',
+      'ClearButton':'Limpar',
+      'FilterButton':'filtrar',
+
+    },
+    'pager': {
+      'currentPageInfo': '{0} de {1} páginas',
+      'totalItemsInfo': '({0} itens)',
+      'firstPageTooltip': 'Vá para a primeira página',
+      'lastPageTooltip': 'Ir para a última página',
+      'nextPageTooltip': 'Vá para a próxima página',
+      'previousPageTooltip': 'Vá para a página anterior',
+      'nextPagerTooltip': 'Ir para o próximo pager',
+      'previousPagerTooltip': 'Vá para o Pager anterior'
+    }
+  }
+});
 @Component({selector: 'app', templateUrl: 'app.component.html', styleUrls: ['./app.component.scss']})
 export class AppComponent {
   currentUser: User;
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@
 import {Dashboard} from '@app/_models/Dashboard';
 import {DashboardService} from '@app/_services/dashboard.service';
 import {ProgressoHome} from "@app/_models/ProgressoHome";
+import {PageSettingsModel} from '@syncfusion/ej2-angular-grids';
+
 
 
 @Component({
@@ -14,30 +16,33 @@ export class HomeComponent implements OnInit {
   progresso: ProgressoHome;
   public datas: Object[];
   public filterSettings: Object;
+  public pageOptions: PageSettingsModel;
+
   constructor(private dashService: DashboardService) {
   }
 
   ngOnInit() {
-    this.filterSettings = { type: 'Menu' };
+    this.filterSettings = {type: 'Menu'};
+    this.pageOptions = {pageSize: 7};
     this.datas = [
-      {OrderID: 10248, CustomerID: 'VINET', Freight: 32.38, ShipCountry: 'France'},
-      {OrderID: 10249, CustomerID: 'TOMSP', Freight: 11.61, ShipCountry: ' Germany'},
-      {OrderID: 10250, CustomerID: 'HANAR', Freight: 65.83, ShipCountry: 'Brazil'},
-      {OrderID: 10251, CustomerID: 'VICTE', Freight: 41.34, ShipCountry: 'France'},
-      {OrderID: 10252, CustomerID: 'SUPRD', Freight: 51.3, ShipCountry: 'Belgium'},
-      {OrderID: 10253, CustomerID: 'HANAR', Freight: 58.17, ShipCountry: 'Brazil'},
-      {OrderID: 10254, CustomerID: 'CHOPS', Freight: 22.98, ShipCountry: 'Switzerland'},
-      {OrderID: 10255, CustomerID: 'RICSU', Freight: 148.33, ShipCountry: 'Switzerland'},
-      {OrderID: 10256, CustomerID: 'SUPRD', Freight: 13.97, ShipCountry: 'Brazil'},
-      {OrderID: 10257, CustomerID: 'WELLI', Freight: 14.23, ShipCountry: 'Venezuela'},
-      {OrderID: 10258, CustomerID: 'VICTE', Freight: 18.33, ShipCountry: 'France'},
-      {OrderID: 10259, CustomerID: 'WELLI', Freight: 28.13, ShipCountry: 'Brazil'},
-      {OrderID: 10260, CustomerID: 'CHOPS', Freight: 48.34, ShipCountry: 'Switzerland'},
-      {OrderID: 10261, CustomerID: 'SUPRD', Freight: 32.73, ShipCountry: ' Germany'},
-      {OrderID: 10262, CustomerID: 'TOMSP', Freight: 12.31, ShipCountry: 'Switzerland'},
-      {OrderID: 10263, CustomerID: 'VICTE', Freight: 23.77, ShipCountry: 'Brazil'},
-      {OrderID: 10264, CustomerID: 'SUPRD', Freight: 43.47, ShipCountry: 'Venezuela'},
-      {OrderID: 10265, CustomerID: 'CHOPS', Freight: 53.37, ShipCountry: 'Belgium'},
+      {ID: 10249, NOME: 'TOMSP', VALOR: 11.61, PAÍS: ' Germany'},
+      {ID: 10250, NOME: 'HANAR', VALOR: 65.83, PAÍS: 'Brazil'},
+      {ID: 10251, NOME: 'VICTE', VALOR: 41.34, PAÍS: 'France'},
+      {ID: 10252, NOME: 'SUPRD', VALOR: 51.3,  PAÍS: 'Belgium'},
+      {ID: 10248, NOME: 'VINET', VALOR: 32.38, PAÍS: 'France'},
+      {ID: 10253, NOME: 'HANAR', VALOR: 58.17, PAÍS: 'Brazil'},
+      {ID: 10254, NOME: 'CHOPS', VALOR: 22.98, PAÍS: 'Switzerland'},
+      {ID: 10255, NOME: 'RICSU', VALOR: 148.33,PAÍS: 'Switzerland'},
+      {ID: 10256, NOME: 'SUPRD', VALOR: 13.97, PAÍS: 'Brazil'},
+      {ID: 10257, NOME: 'WELLI', VALOR: 14.23, PAÍS: 'Venezuela'},
+      {ID: 10258, NOME: 'VICTE', VALOR: 18.33, PAÍS: 'France'},
+      {ID: 10259, NOME: 'WELLI', VALOR: 28.13, PAÍS: 'Brazil'},
+      {ID: 10260, NOME: 'CHOPS', VALOR: 48.34, PAÍS: 'Switzerland'},
+      {ID: 10261, NOME: 'SUPRD', VALOR: 32.73, PAÍS: ' Germany'},
+      {ID: 10262, NOME: 'TOMSP', VALOR: 12.31, PAÍS: 'Switzerland'},
+      {ID: 10263, NOME: 'VICTE', VALOR: 23.77, PAÍS: 'Brazil'},
+      {ID: 10264, NOME: 'SUPRD', VALOR: 43.47, PAÍS: 'Venezuela'},
+      {ID: 10265, NOME: 'CHOPS', VALOR: 53.37, PAÍS: 'Belgium'},
     ];
     this.loading = true;
     this.dashService.listarDash().subscribe(x => this.data = x);
